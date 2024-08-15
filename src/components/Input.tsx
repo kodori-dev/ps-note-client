9b7c4d6a9e1db26f61596d13cc6ed6c79c5b96dd
heewon <hw7666@sogang.ac.kr>
2024-08-15 19:58:35 +0900

    ✨ feat: Input 컴포넌트 구현

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  label: string;
+  placeholder?: string;
+  description?: string;
+  type?: 'password' | 'email';
+  register?: any;
+  error?: any;
+}
+
+const ERR_STYLE = 'border-red focus:border-red';
+
+function Input({ label, placeholder, description, register, error }: Props) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="font-700 flex flex-col">
+        {label}
+        <input
+          {...register}
+          placeholder={placeholder}
+          className={`focus:outline-none focus:border-primary h-[54px] p-4 font-400 border border-gray-4 rounded-md placeholder:text-gray-3 + ${
+            error ? ERR_STYLE : null
+          }`}
+        />
+      </label>
+      {!error && description && <p className="text-gray-2 text-12">{description}</p>}
+      {error && <p className="text-12 text-red">{error.message}</p>}
+    </div>
+  );
+}
+
+export default Input;

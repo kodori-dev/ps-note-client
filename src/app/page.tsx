4fcbc6e2850c2e7ef768ac0614cfeafc675a08d9
heewon <hw7666@sogang.ac.kr>
2024-08-19 01:18:50 +0900

    🐛 fix: 로그인 후 홈페이지 섹션 리렌더링안되는 문제 해결중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,35 @@ import HomeSectionLayout from '@/components/Layout/HomeSectionLayout';
 import SearchBar from '@/components/SearchBar';
 import MemberSection from '@/components/Section/MemberSection';
 import ProblemSection from '@/components/Section/ProblemSection';
+import { cookies } from 'next/headers';
 
-const SECTION_LIST = [
-  { title: '이런 문제를 추천해요!', children: <ProblemSection type="recommended" /> },
-  { title: '오늘은 누가 먼저 놀았을까요?', children: <ProblemSection type="today" /> },
-  { title: '놀이의 전당', children: <MemberSection /> },
-];
+export default async function Home() {
+  const cookie = cookies();
 
-export default function Home() {
+  const getMembers = async () => {
+    try {
+      const res = await fetch(`http://${process.env.NEXT_PUBLIC_API_BASE_URL}/api/members`, {
+        headers: { Cookie: cookie.toString() || '' },
+        cache: 'no-store',
+      });
+      console.log('멤버fetch');
+      if (res.ok) return await res.json();
+      else throw Error();
+    } catch (err) {
+      return null;
+    }
+  };
+
+  console.log('홈페이지 렌더링');
+
+  const members = await getMembers();
+
+  const SECTION_LIST = [
+    { title: '이런 문제를 추천해요!', children: <ProblemSection type="recommended" /> },
+    { title: '오늘은 누가 먼저 놀았을까요?', children: <ProblemSection type="today" /> },
+    { title: '놀이의 전당', children: <MemberSection members={members} /> },
+  ];
+
   return (
     <div className="my-8 flex flex-col gap-12">
       <SearchBar />
--- src/components/Section/MemberSection.tsx
+++ src/components/Section/MemberSection.tsx
@@ -1,29 +1,15 @@
-import { cookies } from 'next/headers';
+'use client';
 import MemberCard from '../Card/MemberCard';
-import { Suspense } from 'react';
 import { GetMembersRes } from '@/types/api/auth';
 import HomeLock from '../Lock/HomeLock';
 
-async function MemberSection() {
-  const cookie = cookies();
+interface Props {
+  members: GetMembersRes | null;
+}
 
-  const getMembers = async () => {
-    try {
-      const res = await fetch(`http://${process.env.NEXT_PUBLIC_API_BASE_URL}/api/members`, {
-        headers: { Cookie: cookie.toString() || '' },
-        cache: 'no-store',
-      });
-      if (res.ok) return await res.json();
-      else throw Error();
-    } catch (err) {
-      return null;
-    }
-  };
-
-  const members = (await getMembers()) as GetMembersRes | null;
-
+function MemberSection({ members }: Props) {
   return (
-    <Suspense fallback={<p>로딩중</p>}>
+    <>
       {members ? (
         <div className="flex gap-7 flex-wrap">
           {members.map(({ id, nickname, boj_id }) => (
@@ -33,7 +19,7 @@ async function MemberSection() {
       ) : (
         <HomeLock type="member" />
       )}
-    </Suspense>
+    </>
   );
 }
 

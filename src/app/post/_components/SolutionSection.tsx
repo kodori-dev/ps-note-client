1c666fd514082da910a37647c504b2645d17d8e6
heewon <hw7666@sogang.ac.kr>
2024-08-20 03:22:58 +0900

    ✨ feat: solution 섹션 완료

---
+++ src/app/post/_components/SolutionSection.tsx
@@ -0,0 +1,81 @@
+'use client';
+import PostSectionLayout from '@/components/Layout/PostSectionLayout';
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { useCallback } from 'react';
+import CodeMirror, { basicSetup } from '@uiw/react-codemirror';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import { cpp } from '@codemirror/lang-cpp';
+import { java } from '@codemirror/lang-java';
+import { javascript } from '@codemirror/lang-javascript';
+import { python } from '@codemirror/lang-python';
+import { rust } from '@codemirror/lang-rust';
+import { LANGUAGE } from '@/constants/language';
+import { LanguageSupport } from '@codemirror/language';
+import { useFormContext } from 'react-hook-form';
+
+function SolutionSection() {
+  const { register, setValue, watch } = useFormContext();
+  const { source_lang, source_code } = watch();
+  const handleCodeChange = useCallback((val: string, viewUpdate: any) => {
+    setValue('source_code', val);
+  }, []);
+
+  const matchLangTheme = (lang: string) => {
+    switch (lang) {
+      case 'C++':
+      case 'C':
+      default:
+        return cpp();
+      case 'Java':
+      case 'Kotlin':
+        return java();
+      case 'JavaScript':
+        return javascript();
+      case 'Python':
+        return python();
+      case 'Rust':
+        return rust();
+    }
+  };
+
+  return (
+    <PostSectionLayout title="Solution" description="정답 코드를 기록해 보세요.">
+      <div className="flex flex-col gap-5">
+        <Menu>
+          <MenuButton
+            rightIcon={<ChevronDownIcon w={6} h={6} />}
+            fontSize={'small'}
+            textAlign={'start'}
+            width="373px"
+            size="lg"
+            backgroundColor={'white'}
+            rounded={'16px'}
+            h={54}
+            as={Button}
+          >
+            {source_lang}
+          </MenuButton>
+          <MenuList>
+            {LANGUAGE.map((lang) => (
+              <MenuItem onClick={() => setValue('source_lang', lang)}>{lang}</MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+        <CodeMirror
+          value={source_code}
+          width="668px"
+          height="330px"
+          onChange={handleCodeChange}
+          theme={vscodeDark}
+          lang="java"
+          extensions={[basicSetup(), matchLangTheme(source_lang) as LanguageSupport]}
+        />
+        <input hidden {...register('source_lang')} />
+        <input hidden {...register('source_code')} />
+      </div>
+    </PostSectionLayout>
+  );
+}
+
+export default SolutionSection;
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -2,18 +2,25 @@
 
 import { FormProvider, useForm } from 'react-hook-form';
 import ProblemSection from './_components/ProblemSection';
+import SolutionSection from './_components/SolutionSection';
+
+const DEFAULT_INPUT = {
+  source_lang: '풀이 언어를 선택하세요.',
+  source_code: '',
+};
 
 function Post() {
-  const methods = useForm({ mode: 'onSubmit' });
+  const methods = useForm({ mode: 'onSubmit', defaultValues: DEFAULT_INPUT });
 
   return (
     <FormProvider {...methods}>
       <form className="flex flex-col gap-16">
         <div className="flex flex-col gap-1">
-          <h1 className="text-48 font-700 text-primary">Today's Check-In</h1>
+          <h1 className="text-48 font-700 text-primary">Today's Check-In✏️</h1>
           <p className="text-12 text-gray-3">* 출석 날짜는 BOJ 기준(06시 초기화)에 맞춰 자동 반영됩니다.</p>
         </div>
         <ProblemSection />
+        <SolutionSection />
       </form>
     </FormProvider>
   );

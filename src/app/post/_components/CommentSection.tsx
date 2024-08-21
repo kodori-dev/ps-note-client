949fff0192ee4e4b1834f0c105c90948621305b6
heewon <hw7666@sogang.ac.kr>
2024-08-22 03:17:16 +0900

    🐛 fix: editor 라이브러리 dynamic import로 변경

--- src/app/post/_components/CommentSection.tsx
+++ src/app/post/_components/CommentSection.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import PostSectionLayout from '@/components/Layout/PostSectionLayout';
-import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
-import 'tui-color-picker/dist/tui-color-picker.css';
-import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import 'prismjs/themes/prism.css';
-import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
+import dynamic from 'next/dynamic';
 import { ForwardedRef } from 'react';
+
+const EditorWrapper = dynamic(() => import('./EditorWrapper'), { ssr: false });
 
 interface Props {
   forwardRef: ForwardedRef<Editor>;
@@ -19,13 +15,7 @@ function CommentSection({ forwardRef }: Props) {
   return (
     <PostSectionLayout title="Comment" description="어떻게 풀었는지 기록해 보세요.">
       <div className="w-[668px] bg-white">
-        <Editor
-          ref={forwardRef}
-          height="400px"
-          placeholder="WA의 경우, 접근법을 상세히 기록해요."
-          initialEditType="markdown"
-          plugins={[codeSyntaxHighlight, colorSyntax]}
-        />
+        <EditorWrapper forwardRef={forwardRef} />
       </div>
     </PostSectionLayout>
   );

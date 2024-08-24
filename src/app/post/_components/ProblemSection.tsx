63ad00dacda3dd1bfedc8c73eded2ce393bd2bd1
heewon <hw7666@sogang.ac.kr>
2024-08-25 04:40:35 +0900

    ✨ feat: 홈페이지 검색 디바운싱 적용

--- src/app/post/_components/ProblemSection.tsx
+++ src/app/post/_components/ProblemSection.tsx
@@ -2,13 +2,12 @@
 
 import Input from '@/components/Input';
 import PostSectionLayout from '@/components/Layout/PostSectionLayout';
+import SearchPreview from '@/components/Search/SearchPreview';
 import Tag from '@/components/Tag';
 import { REQUIRED_INPUT } from '@/constants/errorMsg';
 import { useDebouncingSearch } from '@/hooks/useDebouncingSearch';
-import { api } from '@/utils/api';
-import { Checkbox, Spinner } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { Checkbox } from '@chakra-ui/react';
+import { ChangeEvent } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 function ProblemSection() {
@@ -35,48 +34,7 @@ function ProblemSection() {
       <div className="flex flex-col gap-5 w-[373px]">
         <div className="relative">
           <Input label="백준 번호*" placeholder="번호 또는 이름 검색" register={register('boj_id', { required: REQUIRED_INPUT })} />
-          {isOpen && data && (
-            <div className="w-full absolute top-[91px] bg-white rounded-md shadow-xl p-4 z-menu">
-              <div className="h-[380px] flex flex-col items-center overflow-y-scroll">
-                {isLoading ? (
-                  <Spinner color="blue.500" />
-                ) : isSuccess && data.count > 0 ? (
-                  data?.results.map(({ id, boj_id: bojId, name }) => (
-                    <div
-                      onClick={() => handleListClick(id, bojId, name)}
-                      key={id}
-                      className="w-full flex gap-5 border-b text-gray-1 border-gray-4 text-14 p-3 hover:bg-primary-background hover:text-black cursor-pointer"
-                    >
-                      <p className="w-20">
-                        {bojId.includes(boj_id) ? (
-                          <>
-                            {bojId.slice(0, bojId.indexOf(boj_id))}
-                            <span className="text-primary">{bojId.slice(bojId.indexOf(boj_id), bojId.indexOf(boj_id) + boj_id.length)}</span>
-                            {bojId.slice(bojId.indexOf(boj_id) + boj_id.length)}
-                          </>
-                        ) : (
-                          bojId
-                        )}
-                      </p>
-                      <p>
-                        {name.includes(boj_id) ? (
-                          <>
-                            {name.slice(0, name.indexOf(boj_id))}
-                            <span className="text-primary">{name.slice(name.indexOf(boj_id), name.indexOf(boj_id) + boj_id.length)}</span>
-                            {name.slice(name.indexOf(boj_id) + boj_id.length)}
-                          </>
-                        ) : (
-                          name
-                        )}
-                      </p>
-                    </div>
-                  ))
-                ) : (
-                  `검색 결과가 없습니다.`
-                )}
-              </div>
-            </div>
-          )}
+          {isOpen && <SearchPreview query={boj_id} isLoading={isLoading} isSuccess={isSuccess} data={data} handleListClick={handleListClick} />}
         </div>
         <div className="flex flex-col gap-2 text-14 font-700">
           채점 결과*

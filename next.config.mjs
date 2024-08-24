9e1863814550a0362f1121b3939faaec5d1adfae
tonynamy <tonynamy@apperz.co.kr>
2024-08-25 01:42:26 +0900

    sentry tracing 개선

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,48 @@
 import {withSentryConfig} from '@sentry/nextjs';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  serverRuntimeConfig: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
-  output: 'standalone',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    reactStrictMode: false,
+    serverRuntimeConfig: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    },
+    publicRuntimeConfig: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    },
+    output: 'standalone',
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
 
-    return config;
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/proxy/:path*',
-        destination: `http://${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-      },
-    ];
-  },
+        return config;
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/proxy/:path*',
+                destination: `http://${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+            },
+        ];
+    },
 };
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "kodori",
-project: "ps-note-client",
+    org: "kodori",
+    project: "ps-note-client",
 
 // Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+    silent: !process.env.CI,
 
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+    widenClientFileUpload: true,
 
 // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
@@ -50,14 +51,20 @@ widenClientFileUpload: true,
 // tunnelRoute: "/monitoring",
 
 // Hides source maps from generated client bundles
-hideSourceMaps: true,
+    hideSourceMaps: true,
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+    disableLogger: true,
 
 // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+    automaticVercelMonitors: true,
+}, {
+    // Upload additional client files (increases upload size)
+    widenClientFileUpload: true,
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 });

3e2074ae71aa0a7d684286d774aa72c7d1ba4884
heewon <hw7666@sogang.ac.kr>
2024-08-28 20:27:12 +0900

    💄 design: 오늘 제출한 솔루션 섹션 UI 구현

--- src/app/attend/[id]/_component/TodaySection.tsx
+++ src/app/attend/[id]/_component/TodaySection.tsx
@@ -2,14 +2,32 @@ import ProblemCard from '@/components/Card/ProblemCard';
 import { SolutionType } from '@/types/api/solution';
 
 interface Props {
-  data?: SolutionType[];
+  data: SolutionType[];
 }
 
 function TodaySection({ data }: Props) {
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-32">오늘 제출한 솔루션</h2>
-      <div>카드리스트</div>
+      <h2 className="text-32">
+        <span className="font-700">오늘 제출</span>한 솔루션
+      </h2>
+      <div className="flex flex-nowrap w-[559px] gap-3 overflow-x-scroll scroll-hidden">
+        {data.map(({ problem, is_correct_answer, score_label, source_lang, id }) => (
+          <ProblemCard
+            key={id}
+            type="solution"
+            bojId={problem.boj_id}
+            problemId={problem.id}
+            title={problem.name}
+            isStar={problem.is_starred}
+            stars={problem.stars}
+            isCorrectAnswer={is_correct_answer}
+            resultLabel={score_label}
+            isSolved
+            solLang={source_lang}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/attend/[id]/page.tsx
+++ src/app/attend/[id]/page.tsx
@@ -6,7 +6,6 @@ import { GetPenaltiesRes, PenaltyType } from '@/types/api/penalty';
 import AllTodaySection from './_component/AllTodaySection';
 
 async function Attend({ params: { id } }: { params: { id: string } }) {
-  console.log(id);
   /**
    * 필요 데이터
    * - 전 멤버 & 오늘 페널티 (server)
@@ -28,18 +27,20 @@ async function Attend({ params: { id } }: { params: { id: string } }) {
 
   return (
     <>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-9">
         <h1 className="text-48">
           <span className="font-700">{todayMemberData.member.nickname}</span> 님의 출석 현황
         </h1>
-        <AllTodaySection
-          today={todayMemberData.day}
-          all={todayPenaltyData.length}
-          todayAttendNum={calcPassMemCnt(todayPenaltyData)}
-          attend={todayMemberData.coupons.length > 0 ? 'coupon' : todayMemberData.is_penalty ? 'notYet' : 'already'}
-          nickname={todayMemberData.member.nickname}
-        />
-        {/* <TodaySection /> */}
+        <div className="flex gap-8">
+          <AllTodaySection
+            today={todayMemberData.day}
+            all={todayPenaltyData.length}
+            todayAttendNum={calcPassMemCnt(todayPenaltyData)}
+            attend={todayMemberData.coupons.length > 0 ? 'coupon' : todayMemberData.is_penalty ? 'notYet' : 'already'}
+            nickname={todayMemberData.member.nickname}
+          />
+          <TodaySection data={todayMemberData.admitted_solutions.concat(todayMemberData.not_admitted_solutions)} />
+        </div>
       </div>
       {/* <WeekSection /> */}
     </>

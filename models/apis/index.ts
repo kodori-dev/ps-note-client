1d2ad8392208f678f0e3ed33157a221c4fc38512
heewon <hw7666@sogang.ac.kr>
2024-09-19 04:51:25 +0900

    🐛 fix: openAPI 타입 파일 깃헙 업로드

---
+++ models/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './CouponApi';
+export * from './HolidayApi';
+export * from './MemberApi';
+export * from './PenaltyApi';
+export * from './ProblemApi';
+export * from './ProblemStarApi';
+export * from './SolutionApi';

d2f56ad711dfbae33f2dde901735fa89c31839c7
naya-h2 <hw7666@naver.com>
2025-03-10 04:02:32 +0900

    fix: '는 바로 쓸 수가 없다구 ~

--- src/components/Layout/PostLayout.tsx
+++ src/components/Layout/PostLayout.tsx
@@ -22,7 +22,7 @@ function PostLayout({ type = "post", children, onSubmitFunc, methods }: Props) {
     <FormProvider {...methods}>
       <form className="flex flex-col gap-16 mb-24" onSubmit={onSubmitFunc}>
         <div className="flex flex-col gap-1">
-          <h1 className="text-48 font-700 text-primary mt-11">Today's Check-In</h1>
+          <h1 className="text-48 font-700 text-primary mt-11">{`Today's Check-In`}</h1>
           <p className="text-12 text-gray-3">* 출석 날짜는 BOJ 기준(06시 초기화)에 맞춰 자동 반영됩니다.</p>
         </div>
 

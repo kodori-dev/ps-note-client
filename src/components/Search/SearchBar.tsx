b6fb344dcb6086b33f753970f8ce68dd51cc8e3b
naya-h2 <hw7666@naver.com>
2025-09-25 21:06:19 +0900

    design: 다크모드 버튼 헤더에 추가

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { FormEvent, useEffect } from "react";
-import SearchIcon from "../../../public/icon-search.svg";
 import { useForm } from "react-hook-form";
 import SearchPreview from "./SearchPreview";
 import { useDebouncingSearch } from "@/hooks/useDebouncingSearch";
-import { Input } from "@chakra-ui/react";
-import { InputGroup } from "../ui/input-group";
+import { Input, InputGroup } from "@chakra-ui/react";
+import { IoSearch } from "react-icons/io5";
 
 interface Props {
   initialValue?: string;
@@ -34,8 +33,8 @@ function SearchBar({ initialValue }: Props) {
 
   return (
     <form className="relative max-w-[700px] mx-auto w-full" onSubmit={handleSearchSubmit}>
-      <InputGroup width={"full"} flex="1" startElement={<SearchIcon alt="검색 아이콘" className="absolute left-1" />}>
-        <Input {...register("searchBar")} css={{ "--focus-color": "colors.blue.500" }} variant={"flushed"} placeholder="백준 번호 또는 이름으로 문제를 검색해 보세요." />
+      <InputGroup width={"full"} flex="1" startElement={<IoSearch size={18} color="#3b82f6" aria-label="검색 아이콘" />}>
+        <Input {...register("searchBar")} css={{ "--focus-color": "colors.blue.500" }} variant={"flushed"} placeholder="문제 번호 또는 이름으로 문제를 검색해 보세요." />
       </InputGroup>
       {isOpen && <SearchPreview type="home" query={searchBar} handleListClick={handleListClick} isLoading={isLoading} isSuccess={isSuccess} data={data} />}
     </form>
